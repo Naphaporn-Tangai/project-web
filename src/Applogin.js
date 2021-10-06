be81ab54c1e9cab9f746a33d625028df60411ca1
naphaporn <MadozaGagon@gmail.com>
2021-10-06 13:17:23 +0700

    soft-dev

--- src/Applogin.js
+++ src/Applogin.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
-import { FaUserAlt , FaUnlockAlt } from "react-icons/fa";
+import { FaUserAlt , FaUnlockAlt, FaEye } from "react-icons/fa";
 import './login.css';
 
 function Applogin(){
@@ -7,28 +8,44 @@ function Applogin(){
        
       <div className="form-login">
 
-       <div className="img-login">
-                <img src="https://sites.google.com/site/suratad022544/_/rsrc/1544886981020/phaph-brryakas-ni-sthaban-thekhnoloyi-phracxmkela-cea-khun-thhar-ladkrabang/121700702.jpg?height=213&width=320"/>
-       </div>
-		<div className= "login">
-            <h2>Login</h2>
-            
-            <div className="text-login">
-                <FaUserAlt className="icon-user" />
-                <input type="text"  name="username" id="username" placeholder="Username"/>
-            </div>
-            <div className="text-login">
-                <FaUnlockAlt className="icon-lock" />
-                <input type="password" name="password" id="password" placeholder="Password"/>
+       <div className="flex-login">
+            <div className="img-login">
+                <img src="/image/image 6.png"/>
             </div>
 
-           <div className="btn-login">
-                <input type="submit" value="Login"/>
-           </div>
+            <div className= "login">
+              <div className="form-text">
+                <h2>เข้าสู่ระบบ</h2>
+                
+                <div className="text-login">
+                    <FaUserAlt className="icon" />
+                    <input type="text"  name="username" id="username" placeholder="ชื่อผู้ใช้"/>
+                </div>
 
+                <div className="text-login">
+                    <FaUnlockAlt className="icon-lock" />
+                    <input type="password" name="password" id="password" placeholder="รหัสผ่าน"/>
+                    <span><FaEye className="eye"/></span>
+                </div>
+
+                <div className="forgot-pwd">
+                    <a href="#">ลืมรหัสผ่าน?</a>
+                </div>
+
+                <div className="btn-login">
+                   <input type="submit" value="เข้าสู่ระบบ"/>
+                </div>
+                
+                <div className="register">
+                 <p>ยังไม่ได้เป็นสมาชิก? <a href="#">คลิกเพื่อสมัคร</a></p>
+                </div>
+	            
+             </div>
+                </div>
+               
+            </div> 
+           
         </div>
-	</div>
-
     );
 }
 export default Applogin;
